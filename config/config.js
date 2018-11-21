2c01fca1f8b46c8960ead6d3645e124e29dfb405
Gyanendra Chaubey <gyanendra.chaubey@ge.com>
2018-11-21 23:10:17 +0530

    Heroku app related deployment  changes

--- config/config.js
+++ config/config.js
@@ -15,10 +15,10 @@ module.exports = {
       "dialect": "postgres"
     },
     "production": {
-      "username": "postgres",
-      "password": "postgres",
-      "database": "sequelize",
-      "host": "localhost",
+      "username": "meoojzmihotmkn",
+      "password": "20c76a9791b68b86539d96086e0ab835c10a8cbb77978e42292e3b0cda781614",
+      "database": "d8he08nj7s43h",
+      "host": "ec2-50-17-203-51.compute-1.amazonaws.com",
       "dialect": "postgres"
     }
   
